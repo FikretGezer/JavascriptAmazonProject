d407a9ecab6f45155c97fe3a57fe5c5fcbf34a2f
Mahmut Fikret Gezer <64322071+FikretGezer@users.noreply.github.com>
2023-10-16 16:27:21 +0300

    Amazon project is done.
    Project is done but still it is open to the improvements.

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,48 @@
-const cart = [];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if(!cart)
+{
+    cart = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2
+        }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }];
+}
+export function addToCart(productId)
+{
+    let matchingItem;
+    cart.forEach((cartItem) => {
+        if(productId === cartItem.productId)
+            matchingItem = cartItem;
+    });
+        
+    const quantity = document.querySelector(`.drop-down-quantity-${productId}`).value;
+
+    if(matchingItem)
+        matchingItem.quantity += Number(quantity);
+    else {
+        cart.push({
+            productId,
+            quantity: Number(quantity)
+        });
+    }
+    saveToStorage();
+}
+export function removeFromCart(productId)
+{
+    let newCart = [];
+
+    cart.forEach((cartItem) => {
+        if(cartItem.productId !== productId)
+        {
+            newCart.push(cartItem);
+        }
+    });    
+    cart = newCart;
+    saveToStorage();
+}
+function saveToStorage()
+{
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,28 +1,5 @@
-/*const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];*/
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
 
 const productsGridDiv = document.querySelector('.products-grid');
 products.forEach((product) => {
@@ -76,33 +53,23 @@ products.forEach((product) => {
     </button>
   </div>`;
 });
-const cartQ = document.querySelector('.cart-quantity');
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-    button.addEventListener('click', () => {
-        //const productId = button.dataset.productId;        
-        const productId = button.dataset.productId;        
-        let matchingItem;
-        cart.forEach((item) => {
-            if(productId === item.productId)
-                matchingItem = item;
-        });
-        
-        const quantity = document.querySelector(`.drop-down-quantity-${productId}`).value;
 
-        if(matchingItem)
-            matchingItem.quantity += Number(quantity);
-        else {
-            cart.push({
-                productId,
-                quantity: Number(quantity)
-            });
-        }
 
-        let total = 0;
-        cart.forEach((item) => {
-            total += item.quantity;
+function updateCartQuantity()
+{
+  let total = 0;
+        cart.forEach((cartItem) => {
+            total += cartItem.quantity;
         });
         cartQ.innerHTML = `${total}`;
+}
+const cartQ = document.querySelector('.cart-quantity');
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {     
+        const productId = button.dataset.productId;        
+        addToCart(productId);
+        updateCartQuantity();
+        
     });
 });
 
